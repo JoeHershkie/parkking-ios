6be9173adb8afb3c7eaf1cf79e64b99c7a936254
JoeHershkie <89561488+JoeHershkie@users.noreply.github.com>
2026-07-28 14:13:12 -0400

    Initial Commit

---
+++ Parkking-iOS/ParkkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkkingApp.swift
+//  Parkking
+//
+//  Created by Joey Hershkop on 2026-07-28.
+//
+
+import SwiftUI
+
+@main
+struct ParkkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
